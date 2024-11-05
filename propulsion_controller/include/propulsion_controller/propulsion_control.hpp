c1fcad5de28fbee2236a5edf12a4c9c62f22a588
pwyong <dndyd971113@naver.com>
2024-11-05 18:26:49 +0900

    initial commit

---
+++ propulsion_controller/include/propulsion_controller/propulsion_control.hpp
@@ -0,0 +1,17 @@
+#ifndef PROPULSION_CONTROL
+#define PROPULSION_CONTROL
+
+#include "rclcpp/rclcpp.hpp"
+
+#include "eigen3/Eigen/Core"
+#include "eigen3/Eigen/Dense"
+
+namespace platform_control
+{
+    class PropulsionControl
+    {
+        
+    };
+};
+
+#endif
